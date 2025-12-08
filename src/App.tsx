1bbd735d37e096fdf6f39cffcbf46fe519c980b1
hvignolo@gmail.com <52966611+hvignolo@users.noreply.github.com>
2025-12-07 21:47:03 -0300

    Force unregister service worker to fix caching issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppRouter from './AppRouter';
+import { pwaInstaller } from './pwaInstaller';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    // Force unregister legacy service workers to fix caching issues
+    pwaInstaller.unregisterServiceWorker();
+  }, []);
+
   return <AppRouter />;
 };
 
