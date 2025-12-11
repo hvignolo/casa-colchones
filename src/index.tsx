dc2f039dce74938471b2fb6c0c36af81bf2f40c0
hector fabian vignolo <fvignolo@arandusoft.com>
2025-12-11 09:33:57 -0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -11,3 +11,25 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+// FORCE UNREGISTER SERVICE WORKER
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.ready.then(registration => {
+    registration.unregister();
+  }).catch(error => {
+    console.error(error.message);
+  });
+
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let registration of registrations) {
+      registration.unregister().then((success) => {
+        console.log('Service Worker unregistered:', success);
+      });
+    }
+  });
+}
